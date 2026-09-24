d10f5260819d592a264747c9d568bae612de7c56
workturnedplay <workturnedplay+gitcommits@gmail.com>
2026-09-24 15:30:08 +0200

    can't hurt

--- test.bat
+++ test.bat
@@ -5,6 +5,8 @@ set "NoDefaultCurrentDirectoryInExePath=1"
 ::if running as admin must get back to current dir:
 :: 1. Change directory and store path BEFORE enabling delayed expansion.
 ::    While delayed expansion is disabled, '!' is treated as a literal character.
+setlocal disabledelayedexpansion
+:: ^ needed if called by a 'call me.bat' command where it's already enabled in parent!
 cd /d "%~dp0"
 set "SCRIPT_DIR=%~dp0"
 
